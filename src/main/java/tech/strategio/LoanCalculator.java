ac871bce0700def3a5d8f1f80d38e4d2a6aeddec
Andy E <andy.estevez@strategio.tech>
2022-10-24 12:33:43 -0400

    getRemainingAmount function done

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -7,16 +7,25 @@ public class LoanCalculator {
     /**
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     *
+     *
+     * @param amount used to calculate the remaining for each month
+     * @return the remaining amount after taking 10% for 3 months from the previous month's amount
      */
     static int getRemainingAmountIn3Months(int amount) {
         // TODO: Add your code below
+        int months = 3; int monthly_pay;
+        for(int i = 0; i < months; i++){
+            monthly_pay = amount / 10;
+            amount = amount - monthly_pay;
+        }
+
+        return amount;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter amount: ");
         int amount = scanner.nextInt();
         System.out.println(getRemainingAmountIn3Months(amount));
     }
